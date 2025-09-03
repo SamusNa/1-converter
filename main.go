88c9defcc9a94de5edd89e6397f43c12b81f04b2
SamusNa <avtelenkova@ya.ru>
2025-09-03 11:06:26 +0700

    Конвертер

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	const usdToEUR = 0.8553
 	const usdToRUB = 80.43
-	balanceEUR := 100.0
+	balanceEUR := 115.0
 	var balanceRUB float64
 
 	balanceRUB = balanceEUR/usdToEUR*usdToRUB
