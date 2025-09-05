d897fe637e081cf93f36e341a95b287e05504b55
SamusNa <avtelenkova@ya.ru>
2025-09-05 12:41:26 +0700

    Добавлен ввод пользователя

--- main.go
+++ main.go
@@ -6,6 +6,33 @@ func main() {
 	const usdToEUR = 0.8553
 	const usdToRUB = 80.43
 	const eurToRUB = usdToRUB/usdToEUR
-
+	baseCurrency := getUserInputBase()
+	sum := getUserInputSum()
+	toCurrency := getUserInputToCurrency()
 	fmt.Printf("%.2f RUB\n", eurToRUB)
+}
+
+func converter(baseCurrency string, sum float64, toCurrency string) float64 {
+
+}
+
+func getUserInputBase() string {
+	var baseCurrency string
+	fmt.Print("Введите исходную валюту (EUR, USD или RUB): ")
+	fmt.Scan(&baseCurrency)
+	return baseCurrency
+}
+
+func getUserInputSum () float64 {
+	var sum float64
+	fmt.Print("Введите сумму для конвертации: ")
+	fmt.Scan(&sum)
+	return sum
+}
+
+func getUserInputToCurrency () string {
+	var toCurrency string
+	fmt.Print("Введите целевую валюту (EUR, USD или RUB): ")
+	fmt.Scan(&toCurrency)
+	return toCurrency
 }
