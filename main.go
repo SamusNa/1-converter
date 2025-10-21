fdabfc8b0959d594b271a17a76d8511a6b645a04
SamusNa <avtelenkova@ya.ru>
2025-10-21 19:07:18 +0700

    Добавлена логика конвертера

--- main.go
+++ main.go
@@ -2,37 +2,96 @@ package main
 
 import "fmt"
 
+var baseCurrency string
+const usdToEUR = 0.8553
+const usdToRUB = 80.43
+const eurToRUB = usdToRUB/usdToEUR
+
+
 func main() {
-	const usdToEUR = 0.8553
-	const usdToRUB = 80.43
-	const eurToRUB = usdToRUB/usdToEUR
 	baseCurrency := getUserInputBase()
 	sum := getUserInputSum()
 	toCurrency := getUserInputToCurrency()
-	fmt.Printf("%.2f RUB\n", eurToRUB)
+	result, currency := converter(baseCurrency, sum, toCurrency)
+	//fmt.Printf("%.2f RUB\n", eurToRUB)
+	fmt.Printf("%.2f %v\n", result, currency)
 }
 
-func converter(baseCurrency string, sum float64, toCurrency string) float64 {
-
+func converter(baseCurrency string, sum float64, toCurrency string) (float64, string) {
+	switch baseCurrency {
+	case "USD":
+		if toCurrency == "EUR" {
+			return sum * usdToEUR, "EUR"
+		} else {
+			return sum * usdToRUB, "RUB"
+		}
+	case "EUR":
+		if toCurrency == "USD" {
+			return sum / usdToEUR, "USD"
+		} else {
+			return sum * eurToRUB, "RUB"
+		}
+	default:
+		if toCurrency == "USD" {
+			return sum / usdToRUB, "USD"
+		} else {
+			return sum / eurToRUB, "EUR"
+		}
+	}
 }
 
 func getUserInputBase() string {
-	var baseCurrency string
-	fmt.Print("Введите исходную валюту (EUR, USD или RUB): ")
-	fmt.Scan(&baseCurrency)
-	return baseCurrency
+	for {
+		fmt.Print("Введите исходную валюту (EUR, USD или RUB): ")
+		fmt.Scan(&baseCurrency)
+		switch baseCurrency {
+		case "EUR", "USD", "RUB":
+			return baseCurrency
+		default:
+			fmt.Println("Ошибка: введите исходную валюту заново (EUR, USD или RUB): ")
+		}
+	}
 }
 
-func getUserInputSum () float64 {
+func getUserInputSum() float64 {
 	var sum float64
-	fmt.Print("Введите сумму для конвертации: ")
-	fmt.Scan(&sum)
-	return sum
+	var input string
+	for {
+		fmt.Print("Введите сумму для конвертации: ")
+		fmt.Scan(&input)
+		n, err := fmt.Sscanf(input, "%f", &sum)
+		if err == nil && n == 1 {
+			return sum
+		} else {
+			fmt.Println("Ошибка: введите сумму заново")
+		}
+	}	
 }
 
 func getUserInputToCurrency () string {
 	var toCurrency string
-	fmt.Print("Введите целевую валюту (EUR, USD или RUB): ")
+	for {
+	switch baseCurrency {
+	case "EUR":
+		fmt.Println("Введите целевую валюту (USD или RUB): ")
+	case "USD":
+		fmt.Println("Введите целевую валюту (EUR или RUB): ")
+	case "RUB":
+		fmt.Println("Введите целевую валюту (EUR или USD): ")
+	}
 	fmt.Scan(&toCurrency)
-	return toCurrency
+	switch toCurrency {
+		case "EUR", "USD", "RUB":
+			return toCurrency
+		default:
+			switch baseCurrency {
+			case "EUR":
+				fmt.Println("Ошибка: введите целевую валюту заново (USD или RUB): ")
+			case "USD":
+				fmt.Println("Ошибка: введите целевую валюту заново (EUR или RUB): ")
+			case "RUB":
+				fmt.Println("Ошибка: введите целевую валюту заново (EUR или USD): ")
+			}
+		}
+	}
 }
